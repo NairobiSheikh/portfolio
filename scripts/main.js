2391c78e60a8d2e9047c6a7f8bf31ae294e52f25
Nairobi Sheikh <nairobisheikh24@outlook.com>
2020-10-05 04:06:15 -0700

    All right three days finally got there, tomorrow I shall finish the form and start on the footer.
    I'll also make sure everything is responsive. Added and deleted our folders and images.

--- scripts/main.js
+++ scripts/main.js
@@ -1,76 +1,86 @@
-var typed = new Typed('.typed-text', {
-  strings: ["Software Engineer",  "FullStack Developer", "Freelancer"],
-  typeSpeed: 100,
-  backSpeed: 200
+// ES6 Class
+class TypeWriter {
+  constructor(textElement, words, wait = 3000) {
+    this.textElement = textElement;
+    this.words = words;
+    this.text = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fulltext = this.words[current];
+
+    // Check if deleting
+    if(this.isDeleting) {
+      // Remove char
+      this.text = fulltext.substring(0, this.text.length - 1);
+    } else {
+      // Add char
+      this.text = fulltext.substring(0, this.text.length + 1);
+    }
+
+    // Insert text into element
+    this.textElement.innerHTML = `<span class="text">${this.text}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 300;
+
+    if(this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if(!this.isDeleting && this.text === fulltext) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if(this.isDeleting && this.text === '') {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+  const textElement = document.querySelector('.text-type');
+  const words = JSON.parse(textElement.getAttribute('data-words'));
+  const wait = textElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(textElement, words, wait);
+}
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
 });
-
-
-// // Getting the DOM elements
-// const typedText = document.querySelector('.typed-text');
-// const cursor = document.querySelector(".cursor");
-
-// // Setting up the variable
-// const textArray = ['Software Engineer', 'FullStack Developer', 'Freelancer'];
-// // Setting up a typing delay to 200ms
-// const typingDelay = 200;
-// //Setting up the erasing delay to 100ms
-// const erasingDelay = 100;
-// //Setting-up delay between current and next tex to 2000ms
-// const newTextDelay = 2000;
-// //To keep track;Setting up the current index
-// let textArrayIndex = 0;
-// //To keep track;Setting up the first character
-// let charIndex = 0;
-
-/* Setting up the typing function to type the index array
-for it to wait a second and call itself again to type the 
-second index 
-*/
-
-// function type() {
-//   // Setting up the current index to the length of a right
-//   if (charIndex < textArray[textArrayIndex].length) {
-//     if(!cursor.classList.contains("typing")) cursor.classList.add("typing");
-//     //We're here let's add the next character to the current text content of the class typedText
-//     debugger
-    
-//     typedText.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-//     // typedText.textContent += textArray[textArrayIndex].charAt(charIndex);
-//     //Increase in the car and decks by one
-//     charIndex++;
-//     // Setting up a timer function
-//     setTimeout(type, typingDelay);
-//   }
-//   //Otherwise eraise the current text before displaying the second text 
-//   else {
-//     cursor.classList.remove("typing");
-//   	setTimeout(erase, newTextDelay);
-//   }
-// }
-
-// //Setting up the delay Attribute before calling the erase function
-// function erase() {
-//   // If character index is less than zero remove the character and call the erase function
-// 	if (charIndex > 0) {
-//     if(!cursor.classList.contains("typing")) cursor.classList.add("typing");
-//     //Setting up where the next index is replaced with a cold character index
-//     typedText.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-//     //Decrease character index by one
-//     charIndex--;
-//     setTimeout(erase, erasingDelay);
-//   } 
-//   else {
-//     cursor.classList.remove("typing");
-//     //Increasing the textArray index by one allows it to move to the next string
-//     textArrayIndex++;
-
-//     if(textArrayIndex>=textArray.length) textArrayIndex=0;
-//     setTimeout(type, typingDelay + 1100);
-//   }
-// }
-
-// //Setting up the point of execution aka when the type() gets called
-// document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-//   // Adding some delay
-//   if(textArray.length) setTimeout(type, newTextDelay + 250);
-// });
